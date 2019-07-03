a7669073b9397ed207aa3dea1f2a8018bc955291
Justin Noel <justin.noel@calendee.com>
2019-07-03 06:42:17 -0500

    Initial commit

---
+++ src/components/Menu.tsx
@@ -0,0 +1,45 @@
+import {
+  IonContent,
+  IonHeader,
+  IonIcon,
+  IonItem,
+  IonLabel,
+  IonList,
+  IonMenu,
+  IonMenuToggle,
+  IonTitle,
+  IonToolbar
+} from '@ionic/react';
+import React from 'react';
+import { RouteComponentProps, withRouter } from 'react-router-dom';
+import { AppPage } from '../declarations';
+
+type MenuProps = RouteComponentProps<{}> & {
+  appPages: AppPage[]
+};
+
+const Menu: React.FunctionComponent<MenuProps> = ({ history, appPages }) => (
+  <IonMenu contentId="main">
+    <IonHeader>
+      <IonToolbar>
+        <IonTitle>Menu</IonTitle>
+      </IonToolbar>
+    </IonHeader>
+    <IonContent>
+      <IonList>
+        {appPages.map((appPage, index) => {
+          return (
+            <IonMenuToggle key={index} auto-hide="false">
+              <IonItem routerDirection="root" onClick={() => history.push(appPage.url)}>
+                <IonIcon slot="start" icon={appPage.icon} />
+                <IonLabel>{appPage.title}</IonLabel>
+              </IonItem>
+            </IonMenuToggle>
+          );
+        })}
+      </IonList>
+    </IonContent>
+  </IonMenu>
+);
+
+export default withRouter(Menu);
